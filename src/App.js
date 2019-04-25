d59bc66a437010d99e67d5a7c474b5a2706f5f87
Agoi Abel <agoiabeladeyemi@gmail.com>
2019-04-25 22:02:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    values: []
+  }
+
+  rand = (min, max) =>  {
+    return Math.random() * (max - min) + min;
+  }
+
+  exportF = (elem) => {
+    var table = document.getElementById("table");
+    var html = table.outerHTML;
+    var url = 'data:application/vnd.ms-excel,' + escape(html); // Set your html table into url
+    elem.setAttribute("href", url);
+    elem.setAttribute("download", "export.xls"); // Choose the file name
+    return false;
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const minRef = parseInt(this.minRef.value);
+    const maxRef = parseInt(this.maxRef.value);
+    const rangeRef = parseInt(this.rangeRef.value);
+    const decimalRef = parseInt(this.decimalRef.value);
+
+    const result = [...Array(rangeRef)].map((_, i) => {
+      return this.rand(minRef, maxRef).toFixed(decimalRef);
+    });
+
+    this.setState({
+      values: result
+    });
+  }
+
+  export = () => {
+    this.exportF(this);
+  }
+
+  render() {
+
+    let values = this.state.values.map(value => (
+      <tr>
+        <td>{value}</td>
+      </tr>
+    ))
+
+    return (
+      <div className="App">
+
+
+        <div>
+          <div className={"formGroup"}>
+            <input type="number" ref={input => this.minRef = input} className={"formControl"} placeholder={"Minimum value"} />
+
+            <input type="number" ref={input => this.maxRef = input} className={"formControl"} placeholder={"Maximum value"} />
+
+            <input type="number" ref={input => this.rangeRef = input} className={"formControl"} placeholder={"Range"} />
+
+            <input type="number" ref={input => this.decimalRef = input} className={"formControl"} placeholder={"Decimal"} />
+          </div>
+
+
+          <button onClick={this.handleSubmit} className={"button button_primary"}> Compute </button>
+        </div>
+
+
+        <div className="result">
+          <table id="table">
+              { values }
+          </table>
+        </div>
+
+        <button onClick={this.export} className={"button button_primary"}> Export </button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
